99e2fe9a702101148f2bcbeefe6fab5c8a37a31f
Ryan Volz <rvolz@mit.edu>
2018-03-29 17:31:59 -0400

    Add Windows build to recipe. [skip ci]

---
+++ recipe/bld.bat
@@ -0,0 +1,21 @@
+setlocal EnableDelayedExpansion
+
+:: Make a build folder and change to it
+mkdir build
+cd build
+
+:: configure
+cmake -G "NMake Makefiles" ^
+      -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" ^
+      -DCMAKE_PREFIX_PATH:PATH="%LIBRARY_PREFIX%" ^
+      -DDRF_INSTALL_PREFIX_PYTHON:PATH="%PREFIX%" ^
+      ..
+if errorlevel 1 exit 1
+
+:: build
+nmake
+if errorlevel 1 exit 1
+
+:: install
+nmake install
+if errorlevel 1 exit 1

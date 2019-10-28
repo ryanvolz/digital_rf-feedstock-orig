829c50d33b03a88c00d208165fc52f13b88c866c
Ryan Volz <rvolz@mit.edu>
2019-10-28 17:56:00 -0400

    2.6.3

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
 
-# fix problem building with conda-forge python that doesn't use Anaconda compilers
-if [ ! -z "$GCC" ]; then
-    ln -s "$GCC" "$PREFIX/bin/gcc"
-fi
-
 mkdir build
 cd build
 cmake \
-    -DCMAKE_PREFIX_PATH=$PREFIX \
     -DCMAKE_INSTALL_PREFIX=$PREFIX \
     -DCMAKE_INSTALL_LIBDIR=lib \
     ..
@@ -16,9 +10,3 @@ make
 make test
 rm -r /tmp/hdf5
 make install
-
-# clean up python build fix
-if [ ! -z "$GCC" ]; then
-    rm "$PREFIX/bin/gcc"
-fi
-
